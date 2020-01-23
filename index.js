29639e3a06df5274b55a0f9a8f3f208ba8c8a56c
Pavlienko <computethew@gmail.com>
2020-01-23 19:03:09 +0300

    23.01.2020 WORK

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const gravity = 9.81;
+const gravity = 9.81 * 100000;
+var distances = [];
+var forces = [];
 
 let Circle = function(circleX, circleY, circleRadius, circleDX, circleDY) {
   this.circleX = circleX;
@@ -19,6 +21,7 @@ let Circle = function(circleX, circleY, circleRadius, circleDX, circleDY) {
     ctx.beginPath();
     ctx.fillStyle = "white";
     ctx.arc(this.circleX, this.circleY, this.circleRadius, 0, Math.PI * 2);
+    ctx.fill();
   };
 
   this.update = function() {
@@ -28,28 +31,40 @@ let Circle = function(circleX, circleY, circleRadius, circleDX, circleDY) {
           Math.pow(Math.abs(this.circleY - i.circleY), 2)
       );
 
-      //SIZE CHANGE  NADO DOPELIT!!!!!!
-
-      if (this.circleRadius + i.circleRadius <= distance) {
-        if (this.circleRadius > i.circleRadius) {
-          this.circleRadius += i.circleRadius / 2;
-        } else {
-          this.circleRadius = i.circleRadius + this.circleRadius / 2;
-        }
-      }
-
       let force =
         gravity *
         ((this.circleRadius * i.circleRadius) / Math.pow(distance, 2));
+
+      if (count < 10) {
+        distances.push(distance);
+        forces.push(force);
+        // data.push(distances);
+        // data.push(forces);
+      }
+      count++;
+
+      //SIZE CHANGE  NADO DOPELIT!!!!!!
+
+      // if (this.circleRadius + i.circleRadius <= distance) {
+      //   if (this.circleRadius > i.circleRadius) {
+      //     this.circleRadius += i.circleRadius / 2;
+      //   } else {
+      //     this.circleRadius = i.circleRadius + this.circleRadius / 2;
+      //   }
+      // }
     }
 
     this.draw();
   };
 };
 
+let count = 0;
+
+let data = [];
+
 let circles = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 10; i++) {
   let circleRadius = Math.round(Math.random() * (20 - 5) + 5);
   let circleX = Math.round(
     Math.random() * (canvas.width - 2 * circleRadius) + circleRadius
@@ -64,10 +79,15 @@ for (let i = 0; i < 100; i++) {
 }
 
 console.log(circles);
+console.log(distances);
+console.log(forces);
 
 let animate = function() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  for (let i of circles) {
+    i.update();
+  }
   // requestAnimationFrame(animate);
 };
 
